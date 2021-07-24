479ea8ed06774db0617087e958d19c1034a8f38e
xuyichaoxyc <1817854647@qq.com>
2021-07-24 22:05:06 +0800

    version: a change: @Value 注入具体配置信息

--- springcloudlearninga/src/main/java/com/xyc/springcloudlearninga/controller/HelloController.java
+++ springcloudlearninga/src/main/java/com/xyc/springcloudlearninga/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.xyc.springcloudlearninga.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +19,19 @@ public class HelloController {
     @Autowired
     private Environment env;
 
+    // 注入配置
+    @Value("${server.port}")
+    private String port;
+
     @GetMapping("/hello")
     public String hello() {
 //        return "hello";
         String port = env.getProperty("server.port");
+        return port;
+    }
+
+    @GetMapping("/hello-value")
+    public String helloValue() {
         return port;
     }
 }
