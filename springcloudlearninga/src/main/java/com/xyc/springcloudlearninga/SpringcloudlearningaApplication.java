f6a5b6c8ee566227d6987069ee33a46b1f25f585
xuyichaoxyc <1817854647@qq.com>
2021-07-24 21:39:40 +0800

    version: a change: initialization

---
+++ springcloudlearninga/src/main/java/com/xyc/springcloudlearninga/SpringcloudlearningaApplication.java
@@ -0,0 +1,13 @@
+package com.xyc.springcloudlearninga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudlearningaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudlearningaApplication.class, args);
+    }
+
+}
---
+++ springcloudlearninga/src/test/java/com/xyc/springcloudlearninga/SpringcloudlearningaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyc.springcloudlearninga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudlearningaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
