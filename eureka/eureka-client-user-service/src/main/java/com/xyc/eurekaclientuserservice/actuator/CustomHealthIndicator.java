ba61e8f34740a7e7476227ff3b4803a763ade909
xuyichaoxyc <1817854647@qq.com>
2021-07-26 09:34:30 +0800

    version:0.0.1 change:eureka 健康检查

---
+++ eureka/eureka-client-user-service/src/main/java/com/xyc/eurekaclientuserservice/actuator/CustomHealthIndicator.java
@@ -0,0 +1,21 @@
+package com.xyc.eurekaclientuserservice.actuator;
+
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName CustomHealthIndicator
+ * @Description TODO
+ * @Author admin-xuyichao
+ * @Date 2021/7/26 9:31
+ * @Version 1.0
+ **/
+@Component
+public class CustomHealthIndicator extends AbstractHealthIndicator {
+
+    @Override
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
+        builder.down().withDetail("status", false);
+    }
+}
