f1841cdb77c2c2cbdf09fbebc1f56e351148324a
xuyichaoxyc <1817854647@qq.com>
2021-07-26 09:30:13 +0800

    version:0.0.1 change:eureka EurekaClient，DiscoveryClient 获取信息

--- eureka/eureka-client-user-service/src/main/java/com/xyc/eurekaclientuserservice/controller/UserController.java
+++ eureka/eureka-client-user-service/src/main/java/com/xyc/eurekaclientuserservice/controller/UserController.java
@@ -1,5 +1,12 @@
 package com.xyc.eurekaclientuserservice.controller;
 
+
+
+
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +19,26 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 public class UserController {
+
+    @Qualifier("eurekaClient")
+    @Autowired
+    private EurekaClient eurekaClient;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/user/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/user/infos")
+    public Object serviceUrl() {
+        return eurekaClient.getInstancesByVipAddress("eureka-client-user-service", false);
+    }
+
+    @GetMapping("/user/infos2")
+    public Object serviceUrl2() {
+        return discoveryClient.getInstances("eureka-client-user-service");
     }
 }
